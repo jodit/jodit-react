fec1e5f2f462177d8d575f25f31d914a9681672e
Chupurnov Valeriy <skoder@ya.ru>
2018-05-28 14:12:16 +0500

    Fix https://github.com/jodit/jodit-react/issues/3

---
+++ src/JoditEditor.d.ts
@@ -0,0 +1,11 @@
+import React from 'react';
+
+declare module 'jodit-react' {
+    export interface IJoditEditorProps {
+        value: string,
+        config?: object,
+        onChange: (newValue: string) => void;
+    }
+    const JoditEditor: React.ComponentType<IJoditEditorProps>
+    export default JoditEditor;
+}
