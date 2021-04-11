a9efd9883ee2c18b51440460a635f193af3ae286
xdan <skoder@ya.ru>
2021-04-11 12:32:22 +0300

    New version 1.0.77. Read more https://github.com/jodit/jodit-react/releases/tag/1.0.77

--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,70 @@
 const webpack = require("webpack");
 const path = require('path');
 
-module.exports = (env, argv) => {
-    const debug = !argv || !argv.mode || !argv.mode.match(/production/);
+module.exports = (env, argv, dir = process.cwd()) => {
+	const debug = !argv || !argv.mode || !argv.mode.match(/production/);
 
-    return {
-        entry: './src/index.js',
-        devtool: debug ? 'inline-source-map' : false,
-        module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: [
-                    'babel-loader'
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ],
-            },
-        ]
-    },
+	return {
+		context: dir,
 
-        resolve: {
-            alias: {
-                "jodit-react": './src'
-            },
-        },
+		entry: './src/index.js',
+		devtool: debug ? 'inline-source-map' : false,
 
-        output: {
-            path: path.join(__dirname, '/build/'),
-                filename: 'jodit-react.js',
-                library: 'JoditEditor',
-                libraryTarget: 'umd'
-        },
-        plugins: [
-            new webpack.DefinePlugin({
-                'process.env': {
-                    NODE_ENV: JSON.stringify(debug ? 'development' : 'production'),
-                },
-            }),
-            new webpack.optimize.ModuleConcatenationPlugin(),
-        ],
-        externals: {
-            jodit: 'jodit',
-            Jodit: 'Jodit',
-            react: {
-                root: 'React',
-                    commonjs2: 'react',
-                    commonjs: 'react',
-                    amd: 'react'
-            },
-            'react-dom': {
-                root: 'ReactDOM',
-                    commonjs2: 'react-dom',
-                    commonjs: 'react-dom',
-                    amd: 'react-dom'
-            }
-        }
-    }
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					use: [
+						'babel-loader'
+					]
+				},
+				{
+					test: /\.css$/,
+					use: [
+						'style-loader',
+						'css-loader'
+					],
+				},
+			]
+		},
+
+		resolve: {
+			alias: {
+				"jodit-react": path.join(__dirname, './src')
+			},
+		},
+
+		output: {
+			path: path.join(__dirname, '/build/'),
+			filename: 'jodit-react.js',
+			library: 'JoditEditor',
+			libraryTarget: 'umd'
+		},
+
+		plugins: [
+			new webpack.DefinePlugin({
+				'process.env': {
+					NODE_ENV: JSON.stringify(debug ? 'development' : 'production'),
+				},
+			}),
+			new webpack.optimize.ModuleConcatenationPlugin(),
+		],
+
+		externals: {
+			jodit: 'jodit',
+			Jodit: 'Jodit',
+			react: {
+				root: 'React',
+				commonjs2: 'react',
+				commonjs: 'react',
+				amd: 'react'
+			},
+			'react-dom': {
+				root: 'ReactDOM',
+				commonjs2: 'react-dom',
+				commonjs: 'react-dom',
+				amd: 'react-dom'
+			}
+		}
+	}
 };
