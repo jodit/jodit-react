7d956e2344e38019f1c302d07813825e1169aa95
xdan <skoder@ya.ru>
2022-02-20 23:12:58 +0300

    Added command npm run lint

--- examples/app.js
+++ examples/app.js
@@ -3,7 +3,7 @@ import './app.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import "jodit";
+import 'jodit';
 import 'jodit/build/jodit.min.css';
 import Form from './components/Form';
 
