c7e86f35c36b971c1efa9250b5ebff0dd8814e64
xdan <skoder@ya.ru>
2022-09-06 20:58:33 +0300

    New version 1.3.18. Read more https://github.com/jodit/jodit-react/releases/tag/1.3.18

--- examples/app.js
+++ examples/app.js
@@ -3,13 +3,11 @@ import './app.css';
 import React from 'react';
 import Form from './components/Form';
 
-
 // For React < 18
 // import ReactDOM from 'react-dom';
 // ReactDOM.render(<Form />, document.getElementById('editor'));
 
-
 import { createRoot } from 'react-dom/client';
 const container = document.getElementById('editor');
 const root = createRoot(container);
-root.render(<Form/>);
+root.render(<Form />);
