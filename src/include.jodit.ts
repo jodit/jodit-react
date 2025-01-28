4b2f180f38a5f7c35a5f4db9095c3ab238633041
Lara Sweeney <sweeney.lara@gmail.com>
2025-01-28 15:43:30 +0000

    bugfix(291-chunk-load-error):import from jodit/es2021 instead

--- src/include.jodit.ts
+++ src/include.jodit.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore - Jodit Editor (https://xdsoft.net/jodit/)
-import { Jodit as JoditES5 } from 'jodit/es5/jodit.fat.min';
+import { Jodit as JoditES5 } from 'jodit/es2021/jodit.fat.min';
 import type { Jodit as JoditConstructorType } from 'jodit';
 import 'jodit/es5/jodit.min.css';
 
