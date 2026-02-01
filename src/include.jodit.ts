f1b9a2fa7163f716efcbe41637feda82fbf06ae8
xdan <chupurnov@gmail.com>
2026-02-01 14:09:26 +0300

    New version 5.3.18. Read more https://github.com/jodit/jodit-react/releases/tag/5.3.18

--- src/include.jodit.ts
+++ src/include.jodit.ts
@@ -1,7 +1,7 @@
-import { Jodit as JoditES5 } from 'jodit/esm/index';
+import { Jodit as JoditES5 } from 'jodit/esm/index.js';
 import type { Jodit as JoditConstructorType } from 'jodit/esm/jodit';
 import 'jodit/es2021/jodit.min.css';
 
-import 'jodit/esm/plugins/all';
+import 'jodit/esm/plugins/all.js';
 
 export const Jodit: typeof JoditConstructorType = JoditES5;
